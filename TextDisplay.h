3293b0a39485568bfe293e045bcf91e1d88fa512
LunkyWonky <luzhu9572@gmail.com>
2025-07-25 00:15:46 -0400

    oops (accidently wrote the wrong file in)

--- TextDisplay.h
+++ TextDisplay.h
@@ -1,37 +1,22 @@
-#ifndef __WINDOW_H
-#define __WINDOW_H
-#include <X11/Xlib.h>
+#ifndef TEXTDISPLAY_H
+#define TEXTDISPLAY_H
+
 #include <iostream>
+#include <vector>
 #include <string>
+#include "Observer.h"
+#include "AttackState.h"
+#include "Info.h"
 
-class Xwindow {
-  Display *d;
-  Window w;
-  int s, width, height;
-  GC gc;
-  unsigned long colours[5];
+class TextDisplay : public Observer {
+    std::vector<std::vector<char>> display;
+    const int boardSize;
 
- public:
-  Xwindow(int width=500, int height=500);  
-  ~Xwindow();                              
+public:
+    TextDisplay(std::string setupString, int n);
+    void notify(Subject &sender) override;
 
-  enum {White=0, Black, Red, Green, Blue, B, ChessWhite, Brown}; 
-
-  int getWidth() const;
-  int getHeight() const;
-
-  // Draws a rectangle
-  void fillRectangle(int x, int y, int width, int height, int colour);
-
-  // Draws a string
-  void drawString(int x, int y, std::string msg);
-
-  void drawPawn(int x, int y, int colour);
-  void drawKnight(int x, int y, int colour);
-  void drawBishop(int x, int y, int colour);
-  void drawRook(int x, int y, int colour);
-  void drawQueen(int x, int y, int colour);
-  void drawKing(int x, int y, int colour);
+    friend std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
 };
 
-#endif
+std::ostream &operator<<(std::ostream &out, const TextDisplay &td);
